f2e6004c556f7bfcb2d25826c08f88059418b141
perkles <guilhermealves5194@gmail.com>
2025-05-11 21:26:42 -0300

    Daily Study

---
+++ src/secao05/CondicionalTernaria.java
@@ -0,0 +1,12 @@
+package secao05;
+
+public class CondicionalTernaria {
+    public static void main(String[] args) {
+        double preco = 30;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        System.out.println(desconto);
+        preco = 10;
+        desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        System.out.println(desconto);
+    }
+}
