dbfe0553d19206cbe84240f462a585858d1a0587
perkles <guilhermealves5194@gmail.com>
2025-05-11 22:02:01 -0300

    fixing class name

--- src/secao05/CondicionalTernariaExemplo.java
+++ src/secao05/CondicionalTernariaExemplo.java
@@ -1,6 +1,6 @@
 package secao05;
 
-public class CondicionalTernaria {
+public class CondicionalTernariaExemplo {
     public static void main(String[] args) {
         double preco = 30;
         double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
