278a76c3d9fb8843b67a532435cf1c7d0ad0d018
perkles <guilhermealves5194@gmail.com>
2025-05-17 19:03:54 -0300

    Daily Study

---
+++ src/secao08/entities/TriangleAreaCalculator.java
@@ -0,0 +1,13 @@
+package secao08.entities;
+
+public class TriangleAreaCalculator {
+    public double a;
+    public double b;
+    public double c;
+
+    public double area(){
+        double p =  (a + b + c) / 2.0;
+        return Math.sqrt(p*(p -a)*(p -b)*(p -c));
+
+    }
+}
