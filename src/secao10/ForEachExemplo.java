847d171070c5f88d0151e528f43b08be055608be
perkles <guilhermealves5194@gmail.com>
2025-05-25 16:57:20 -0300

    Daily Study

---
+++ src/secao10/ForEachExemplo.java
@@ -0,0 +1,16 @@
+package secao10;
+
+public class ForEachExemplo {
+    public static void main(String[] args) {
+
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+        for (int i=0; i< vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+        System.out.println("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+");
+
+        for (String obj: vect) {
+            System.out.println(obj);
+        }
+    }
+}
