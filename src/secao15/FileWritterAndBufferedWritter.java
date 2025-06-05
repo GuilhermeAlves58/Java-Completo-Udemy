38bc80243f26f85d8057158f0c4863d8754498d0
perkles <guilhermealves5194@gmail.com>
2025-06-05 18:20:01 -0300

    Daily study

---
+++ src/secao15/FileWritterAndBufferedWritter.java
@@ -0,0 +1,23 @@
+package secao15;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWritterAndBufferedWritter {
+    public static void main(String[] args) {
+
+        String[] lines = new String[]{"Good Morning","Good afternoon","GoodNight"};
+        String path = "c:\\temp\\out.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))) {
+            for (String line: lines){
+                bw.write(line);
+                bw.newLine();
+            }
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
