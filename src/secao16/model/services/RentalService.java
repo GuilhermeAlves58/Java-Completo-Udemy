3f19c7e9a5c724cc40a68856a82d22983a6615ba
perkles <guilhermealves5194@gmail.com>
2025-06-15 18:01:57 -0300

    .

--- src/secao16/model/services/RentalService.java
+++ src/secao16/model/services/RentalService.java
@@ -3,6 +3,8 @@ package secao16.model.services;
 import secao16.model.entities.CarRental;
 import secao16.model.entities.Invoice;
 
+import java.time.Duration;
+
 public class RentalService {
     private Double pricePerDay;
     private Double pricePerhour;
@@ -16,6 +18,19 @@ public class RentalService {
     }
 
     public void processInvoice(CarRental carRental){
-        carRental.setInvoice(new Invoice(50,10));
+        double minutes = Duration.between(carRental.getStart(),carRental.getFinish()).toMinutes();
+        double hours = minutes/60.0;
+
+        double basicPayment;
+        if ( hours <= 12.00){
+            basicPayment = pricePerhour *Math.ceil(hours);
+        }
+        else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment,tax));
     }
 }
