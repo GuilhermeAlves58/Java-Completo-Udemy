2e9a19d6ae0fbcfeada1a779ff95b6bbe128c912
perkles <guilhermealves5194@gmail.com>
2025-06-17 17:09:30 -0300

    daily study

--- src/secao16/ExerFixacao/servicesContract/OnlinePaymentService.java
+++ src/secao16/ExerFixacao/servicesContract/OnlinePaymentService.java
@@ -1,8 +1,6 @@
 package secao16.ExerFixacao.servicesContract;
 
 public interface OnlinePaymentService {
-
     double paymentFee(double amount);
     double interrest(double amount, int moths);
-
 }
