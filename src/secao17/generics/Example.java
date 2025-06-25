9ecd28bf99acbb11f39749bb332a48e9b195be62
perkles <guilhermealves5194@gmail.com>
2025-06-25 17:31:11 -0300

    daily study

---
+++ src/secao17/generics/Example.java
@@ -0,0 +1,28 @@
+package secao17.generics;
+
+import secao17.generics.service.PrintService;
+
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            PrintService<Integer> ps = new PrintService<>();
+
+            System.out.print("How many values? ");
+            int n = sc.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                Integer value = sc.nextInt();
+                ps.addValue(value);
+            }
+
+            ps.print();
+            Integer x = ps.first();
+            System.out.println("First: " + x);
+
+            sc.close();
+    }
+}
