5ec645cd3c166cf8c4052d70bd09c43b48dd4ac4
perkles <guilhermealves5194@gmail.com>
2025-07-01 17:21:40 -0300

    Daily study. My answer

---
+++ src/secao17/set/solvedExer/MainSetSolved.java
@@ -0,0 +1,40 @@
+package secao17.set.solvedExer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MainSetSolved {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Set<UserSet> set = new TreeSet<>();
+
+        System.out.print("Enter the file path: ");
+        String path = sc.next();
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
+            String line = bufferedReader.readLine();
+            while (line != null){
+                String[] fields = line.split(" ");
+                String name = fields[0];
+                String date = fields[1];
+                UserSet userSet = new UserSet(name,date);
+                set.add(userSet);
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error " + e.getMessage());;
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage());;
+        }
+        System.out.println("Total users: " + set.size());
+        for (UserSet user : set){
+            System.out.println("User " + user.getName() +" "+ user.getDate());
+        }
+    }
+}
---
+++ src/secao17/set/solvedExer/UserSet.java
@@ -0,0 +1,50 @@
+package secao17.set.solvedExer;
+
+
+import java.util.Objects;
+
+public class UserSet implements Comparable<UserSet> {
+    private String name;
+    private String date;
+
+    public UserSet(String name, String date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserSet userSet = (UserSet) o;
+        return Objects.equals(name, userSet.name) && Objects.equals(date, userSet.date);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(date);
+        return result;
+    }
+
+    @Override
+    public int compareTo(UserSet o) {
+        return name.compareTo(o.name);
+    }
+}
